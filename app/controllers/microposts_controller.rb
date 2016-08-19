67cb0d2c701a7e19868eb8e0a76a847f982da3cc
klein0118 <danielklein0118@gmail.com>
2016-08-19 17:30:27 +0200

    Build the CRUD in micropost controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,53 @@
 class MicropostsController < ApplicationController
+  def index
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.all
+  end
+
+  def new
+    @user = User.find(params[:id])
+    @micro = Micropost.new
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @micro = @user.micro.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @micro = @user.micro.find(params[:id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @micro = @user.microposts.create(micro_params)
+    if @micro.save
+      redirect_to user_micropost_path(@user, @micro)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @micro = @user.microposts.update(micro_params)
+    if @micro
+      redirect_to user_micropost_path(@user, @micro)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @micro = @user.micro.find(params[:id])
+    @micro.destroy
+    redirect_to user_microposts_path(@user)
+  end
+
+  private
+    def micro_params
+      params.require(:micropost).permit(:content)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :Microposts
   validates :name, :email, presence: true,
                     length: {minimum: 5} 
 end
