40420c87eb80ab8591174b0174124c12fe20dbaa
klein0118 <danielklein0118@gmail.com>
2016-08-20 16:24:04 +0200

    Create the Welocme page and import the Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root "welcome#index"
   resources :users do
-    root "users#index" 
     resources :microposts
   end
   # Example of regular route:
