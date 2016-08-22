32333b773006126bc733f030944ce3fe36f6ae18
klein0118 <danielklein0118@gmail.com>
2016-08-22 18:09:18 +0200

    Create Password for Signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
   has_many :microposts
-  validates :name, :email, presence: true,
-                    length: {minimum: 5} 
+  validates :name, presence: true,
+                    length: {minimum: 5}
+
+  before_save {self.email = email.downcase}
+  # before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }                    
 end
