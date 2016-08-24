94826136620c951fbfab6ebcf960a26506ce4bb0
klein0118 <danielklein0118@gmail.com>
2016-08-24 16:56:16 +0200

    Create remember action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+  
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       log_in user
+      # remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash[:danger] = "Invalid Email and Password "
